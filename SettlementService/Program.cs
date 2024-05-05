0ee64ccdcfc321c6974e7563ae535e3ab3cfa271
LunaYang820 <120531574+LunaYang820@users.noreply.github.com>
2024-05-05 10:35:52 +1000

    Update Program.cs

--- SettlementService/Program.cs
+++ SettlementService/Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IBookingService, BookingService>();
 builder.Services.AddSingleton<IBookingRepository, BookingRepository>();
 
